7890d24da369375c201484b6f3f848166b9d694f
Igor Reidler <igormail@gmail.com>
2020-05-16 19:22:05 +0300

    Before translation 0.06

--- iss_sim_autopilot/iss_sim_autopilot.py
+++ iss_sim_autopilot/iss_sim_autopilot.py
@@ -13,7 +13,8 @@ class controlPanelClass():
         self.desiredRateArray=np.zeros(7)
         self.deltaRateArray=np.zeros(7)
         self.desiredClicksArray=np.zeros(7)
-        self.clicksExecuteArray=np.zeros(7)
+        self.currentClicksArray=np.zeros(7)
+        self.executeClicksArray=np.zeros(7)
         self.rateParamsArray=np.zeros(7)
         self.ratePerClickArray=np.zeros(7)
 
@@ -22,8 +23,8 @@ class controlPanelClass():
         self.timeCurrentErrorUpdate=0
         self.firstreadInstruments=True
         self.ratePerClickRotation=0.1
-        self.ratePerClickTranslation=0.05
-        self.rateGravity=0.0065
+        self.ratePerClickTranslation=0.06
+        #self.rateGravity=0.0065
         self.rotationRateParam=0.03
         self.translationRateParam=0.015
         self.rateParamsArray=[self.rotationRateParam,self.rotationRateParam,self.rotationRateParam,-self.translationRateParam,-self.translationRateParam,-self.translationRateParam,-self.translationRateParam]
@@ -31,7 +32,6 @@ class controlPanelClass():
         #array [roll, pitch, yaw, x, y, z, range]
 
     def readInstruments(self):
-        print('TheLoop: reading instruments ..')
         self.currentErrorArray[0] = float(browser.find_element_by_xpath("//div[@id='roll']/div[@class='error']").text[:-1])
         self.currentErrorArray[1]  =float(browser.find_element_by_xpath("//div[@id='pitch']/div[@class='error']").text[:-1])
         self.currentErrorArray[2]  =float(browser.find_element_by_xpath("//div[@id='yaw']/div[@class='error']").text[:-1])
@@ -51,43 +51,43 @@ class controlPanelClass():
         #self.desiredRateArray=np.power(self.currentErrorArray,2) #for power law rate
         self.desiredRateArray=np.multiply(self.currentErrorArray,self.rateParamsArray)
         self.deltaRateArray=np.subtract(self.desiredRateArray,self.currentRateArray)
-        self.clicksExecuteArray=np.divide(self.deltaRateArray,self.ratePerClickArray)
-        self.clicksExecuteArray=np.clip(self.clicksExecuteArray,-10,10)
-        self.clicksExecuteArray=np.sign(self.clicksExecuteArray) * np.ceil(np.abs(self.clicksExecuteArray))      
-        #self.clicksExecuteArray=np.ceil(self.clicksExecuteArray) #was around
-        self.clicksExecuteArray=self.clicksExecuteArray.astype(int)
-        #print('calcClicksArray desided on =',self.clicksExecuteArray)
+        self.executeClicksArray=np.divide(self.deltaRateArray,self.ratePerClickArray)
+        self.executeClicksArray=np.clip(self.executeClicksArray,-10,10)
+        self.executeClicksArray=np.sign(self.executeClicksArray) * np.ceil(np.abs(self.executeClicksArray))      
+        #self.executeClicksArray=np.ceil(self.executeClicksArray) #was around
+        self.executeClicksArray=self.executeClicksArray.astype(int)
+        #print('calcClicksArray desided on =',self.executeClicksArray)
     def clickButtonsArray(self):
-        print('TheLoop: clicking Buttons ',self.clicksExecuteArray)
-        if self.clicksExecuteArray[0]>0:
-            self.clickButton('roll-right-button',np.absolute(self.clicksExecuteArray[0]),timeDeltaSameClicks)
+        self.currentClicksArray=np.add(self.currentClicksArray,self.executeClicksArray)
+        if self.executeClicksArray[0]>0:
+            self.clickButton('roll-right-button',np.absolute(self.executeClicksArray[0]),timeDeltaSameClicks)
         else: 
-            self.clickButton('roll-left-button',np.absolute(self.clicksExecuteArray[0]),timeDeltaSameClicks)
+            self.clickButton('roll-left-button',np.absolute(self.executeClicksArray[0]),timeDeltaSameClicks)
 #
-        if self.clicksExecuteArray[1]>0:
-            self.clickButton('pitch-down-button',np.absolute(self.clicksExecuteArray[1]),timeDeltaSameClicks)
+        if self.executeClicksArray[1]>0:
+            self.clickButton('pitch-down-button',np.absolute(self.executeClicksArray[1]),timeDeltaSameClicks)
         else: 
-            self.clickButton('pitch-up-button',np.absolute(self.clicksExecuteArray[1]),timeDeltaSameClicks)
+            self.clickButton('pitch-up-button',np.absolute(self.executeClicksArray[1]),timeDeltaSameClicks)
 #
-        if self.clicksExecuteArray[2]>0:
-            self.clickButton('yaw-right-button',np.absolute(self.clicksExecuteArray[2]),timeDeltaSameClicks)
+        if self.executeClicksArray[2]>0:
+            self.clickButton('yaw-right-button',np.absolute(self.executeClicksArray[2]),timeDeltaSameClicks)
         else: 
-            self.clickButton('yaw-left-button',np.absolute(self.clicksExecuteArray[2]),timeDeltaSameClicks)
+            self.clickButton('yaw-left-button',np.absolute(self.executeClicksArray[2]),timeDeltaSameClicks)
 
-        #if self.clicksExecuteArray[4]>0:
-        #    self.clickButton('translate-right-button',np.absolute(self.clicksExecuteArray[5]),timeDeltaSameClicks)
+        #if self.executeClicksArray[4]>0:
+        #    self.clickButton('translate-right-button',np.absolute(self.executeClicksArray[5]),timeDeltaSameClicks)
         #else: 
-        #    self.clickButton('translate-left-button',np.absolute(self.clicksExecuteArray[5]),timeDeltaSameClicks)
+        #    self.clickButton('translate-left-button',np.absolute(self.executeClicksArray[5]),timeDeltaSameClicks)
 ##
-        #if self.clicksExecuteArray[5]>0:
-        #    self.clickButton('translate-up-button',np.absolute(self.clicksExecuteArray[4]),timeDeltaSameClicks)
+        #if self.executeClicksArray[5]>0:
+        #    self.clickButton('translate-up-button',np.absolute(self.executeClicksArray[4]),timeDeltaSameClicks)
         #else: 
-        #    self.clickButton('translate-down-button',np.absolute(self.clicksExecuteArray[4]),timeDeltaSameClicks)
+        #    self.clickButton('translate-down-button',np.absolute(self.executeClicksArray[4]),timeDeltaSameClicks)
 ##
-        #if self.clicksExecuteArray[6]>0:
-        #    self.clickButton('translate-backward-button',np.absolute(self.clicksExecuteArray[6]),timeDeltaSameClicks)
+        #if self.executeClicksArray[6]>0:
+        #    self.clickButton('translate-backward-button',np.absolute(self.executeClicksArray[6]),timeDeltaSameClicks)
         #else: 
-        #    self.clickButton('translate-forward-button',np.absolute(self.clicksExecuteArray[6]),timeDeltaSameClicks)
+        #    self.clickButton('translate-forward-button',np.absolute(self.executeClicksArray[6]),timeDeltaSameClicks)
     def clickButton(self,buttonId,timesNum,timeDeltaSameClicks):
         self.buttonElement=browser.find_element_by_id(buttonId)
         for idx in range(timesNum):
@@ -138,16 +138,18 @@ controlPanel=controlPanelClass() # init controlPanelClass
 
 #The loop
 while 1:
-    print(".... Running the loop ....")
+    print('.... Running the loop ....')
+    print('TheLoop: reading instruments ..')
     controlPanel.readInstruments()
     controlPanel.calcClicksArray()
+    print('The loop: current clicks = ',controlPanel.currentClicksArray)
+    print('TheLoop: executing clicks = ',controlPanel.executeClicksArray)
     controlPanel.clickButtonsArray()
-
     #print('desiredRateArray = currentErrorArray * rateParamsArray')
     #print(controlPanel.desiredRateArray[4],'=',controlPanel.currentErrorArray[4],'*',controlPanel.rateParamsArray[4])
     #print('deltaRateArray = desiredRateArray - currentRateArray')
     #print(controlPanel.deltaRateArray[4],'=',controlPanel.desiredRateArray[4],'-',controlPanel.currentRateArray[4])
-    #print('clickExecute = ',controlPanel.clicksExecuteArray[4])
+    #print('clickExecute = ',controlPanel.executeClicksArray[4])
     #print('done')
 
     #TODO: bad current rate calculation - simply record number of clicks! and test click rate - easy!
